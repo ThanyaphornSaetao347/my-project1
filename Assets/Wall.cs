0c10624e2f92402eb1435109deebe8d42b50dc64
ThanyaphornSaetao347 <tunyaporn4875@gmail.com>
2024-07-18 17:30:54 +0700

    first commit

---
+++ Assets/Wall.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public GameObject block;
+    public int width = 10;
+    public int height = 4;
+
+
+    void create_wall()
+    {
+        for (int y=0; y<height; ++y)
+        {
+            for (int x=0; x<width; ++x)
+            {
+                Vector3 offset = new Vector3(x, y, 0);
+                Instantiate(block, transform.position + offset, Quaternion.identity);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.create_wall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
